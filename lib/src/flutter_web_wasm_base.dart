7c5ce53964c989ff919dbf837513139eaf16977a
Sach97 <sacha.arbonel@hotmail.fr>
2019-09-18 19:25:31 +0200

    publish package + import from cdn as module

--- lib/src/flutter_web_wasm_base.dart
+++ lib/src/flutter_web_wasm_base.dart
@@ -5,4 +5,12 @@ import "package:js/js.dart";
 
 /// tslint:disable
 @JS()
-external void greet();
+external void main();
+@JS()
+external num add(num a, num b);
+
+/// If `module_or_path` is {RequestInfo}, makes a request and
+/// for everything else, calls `WebAssembly.instantiate` directly.
+@JS()
+external Future init(
+    [dynamic /*RequestInfo|BufferSource|WebAssembly.Module*/ module_or_path]);
