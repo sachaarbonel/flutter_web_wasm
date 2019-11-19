96df200ea0341437639db464f28dda4af9be1e97
sacha <sacha.arbonel@hotmail.fr>
2019-11-19 20:57:56 +0100

    wip still crashing

--- lib/src/flutter_web_wasm_base.dart
+++ lib/src/flutter_web_wasm_base.dart
@@ -2,6 +2,7 @@
 library pkg.flutter_web_wasm;
 
 import "package:js/js.dart";
+import 'dart:js_util';
 
 /// tslint:disable
 @JS()
@@ -12,5 +13,5 @@ external num add(num a, num b);
 /// If `module_or_path` is {RequestInfo}, makes a request and
 /// for everything else, calls `WebAssembly.instantiate` directly.
 @JS()
-external Future init(
+external Promise<dynamic> init(
     [dynamic /*RequestInfo|BufferSource|WebAssembly.Module*/ module_or_path]);
